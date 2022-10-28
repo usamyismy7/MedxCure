5ddf3750054270fbf415779b0078339f127d8da5
Usama Qazi <usamyharis@yahoo.com>
2022-10-28 22:11:24 +0500

    commit

--- src/Components/Pages/NotFoundPage.js
+++ src/Components/Pages/NotFoundPage.js
@@ -1,53 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+        <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl">
+          404
+        </h1>
+        <h1 className="block text-2xl font-bold text-gray-800">Not Found</h1>
+        <p className="mt-3 text-gray-600">Oops, something went wrong.</p>
+        <p className="text-gray-600">Sorry, we couldn't find your page.</p>
+        <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+          <Link
+            className="w-full sm:w-auto inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800"
+            to="/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              class="w-6 h-6"
+            >
+              <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+              <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+            </svg>
+            &nbsp;Back To Home
+          </Link>
+          <Link
+            class="w-full sm:w-auto inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold text-blue-500 hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm py-3 px-4 dark:ring-offset-slate-900"
+            to="/"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+              />
+            </svg>
+            &nbsp;Support
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-// import React, { Component } from 'react'
-// import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
-
-// export default class NotFoundPage extends Component {
-//   render() {
-//     return (
-//       <ComponentNotFound className='container'>
-//         <div className='row'>
-//           <div className='col-md-12'>
-//             <div className="error-template">
-//               <h1>Ops!</h1>
-//               <h2>Error 404 Not Found</h2>
-//               <div className='error-details'>
-//                 Sorry, an error occurred. The requested page was not found!
-//               </div>
-//               <div className='error-actions'>
-//                 <Link to="/" className='btn btn-outline-primary btn-lg'>
-//                   <i className='fas fa-home' />
-//                   &nbsp;Back To Home
-//                 </Link>
-//                 <Link to="/" className='btn btn-outline-secondary btn-lg'>
-//                   <i className='fas fa-envelope' />
-//                   &nbsp;Support
-//                 </Link>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </ComponentNotFound>
-//     )
-//   }
-// }
-
-// const ComponentNotFound = styled.div`
-//   .error-template {
-//     padding: 40px 15px;
-//     text-align: center;
-//   }
-//   .error-actions {
-//     margin-top: 15px;
-//     margin-bottom: 15px;
-//   }
-//   .btn {
-//     margin-right: 10px;
-//   }
-// `;
